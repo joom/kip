f411ac5d0f8fe770591f584485a15e85d7d9aca3
Joomy Korkut <joomy@cs.princeton.edu>
2021-01-29 11:22:13 -0500

    Styling in C

--- c/morphology.c
+++ c/morphology.c
@@ -6,16 +6,16 @@
 
 char **ups(struct fsm* fsm, char *s)
 {
-
   char **arr = (char **) calloc(LIMIT, LIMIT * sizeof(char *));
   struct apply_handle *h = apply_init(fsm);
   bool once = false;
+
   for(int i = 0; i < LIMIT; i++)
   {
     char *result = apply_up(h, once ? NULL : s);
-    if (result == NULL) {
-      break;
-    } else {
+    if (result == NULL) break;
+    else 
+    {
       once = once || true;
       arr[i] = strdup(result);
     }
